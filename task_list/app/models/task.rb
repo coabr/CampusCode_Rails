dae9799e879ee33676d3e51a593030ccf4b707bf
COAB1 <carol.o.ag@gmail.com>
2020-07-27 08:38:28 -0300

    Altera o controller

--- task_list/app/models/task.rb
+++ task_list/app/models/task.rb
@@ -1,2 +1,7 @@
-class Task < ApplicationRecord
+class HomeController < ApplicationController  
+    def welcome    
+        @tasks = []    
+        @tasks << Task.new(description: 'Comprar pão')    
+        @tasks << Task.new(description: 'Comprar leite')  
+    end
 end
