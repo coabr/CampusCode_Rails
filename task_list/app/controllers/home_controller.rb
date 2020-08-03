180a19976c7b0de912370c33d2d03e9ca6ba7f42
COAB1 <carol.o.ag@gmail.com>
2020-08-03 14:17:23 -0300

    adiciona taks a lista de tarefas

--- task_list/app/controllers/home_controller.rb
+++ task_list/app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
         @tasks = []    
         @tasks << Task.new('Comprar pão')    
         @tasks << Task.new('Comprar leite')
+        @tasks << Task.new('Comprar ovo')
+        @tasks << Task.new('Comprar presunto')
     end
 end
